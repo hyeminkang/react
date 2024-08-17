3684cee925099ee515b906bdbd7fccb0a1965f18
hamie.kang <hamie.kang@kakaocorp.com>
2024-08-17 18:52:24 +0900

    ~5강

--- react_ui/src/App.js
+++ react_ui/src/App.js
@@ -1,11 +1,15 @@
-
+import Button from './Button'
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
-      <h1>welcome back!!!</h1>
+      <h1 className={styles.title}>welcome back!!!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
 
 export default App;
+
+// create-react-app 클래스 네임은 화면확인시에 같아도 각자 뿌려줌, 다를수있음
