fdbeeb74b30b0b1e28276958a2ceabd230733c7f
hamie.kang <hamie.kang@kakaocorp.com>
2024-09-03 18:44:40 +0900

    no message

--- react_ui/src/App.js
+++ react_ui/src/App.js
@@ -7,14 +7,13 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() { 
-  return (
-    <Router>
+  return 
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
-  );
 } 
 export default App;
 
